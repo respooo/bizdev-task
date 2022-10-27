938619fe2bf1b67dbc5e856eadf835d594756348
tenmakamatani <tenmakamatani0@gmail.com>
2022-10-27 14:28:18 +0900

    send message to slack

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Client } = require('@notionhq/client');
 const Dotenv = require('dotenv');
+const { WebClient } = require('@slack/web-api');
 
 const main = async () => {
     Dotenv.config();
@@ -11,7 +12,14 @@ const main = async () => {
     const now = new Date();
     const todayKey = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
     const todayTasks = res.results.filter((result) => result.properties['期限'].date?.start === todayKey);
-    console.log(todayTasks.length);
+
+    const slackClient = new WebClient(process.env.SLACK_API_TOKEN);
+    await slackClient.chat.postMessage({
+        channel: '#biz-all',
+        text:
+            `今日締め切りのタスクがあります！\n` +
+            todayTasks.map((todayTask) => `<${todayTask.url}|${todayTask.properties['名前'].title[0]?.plain_text ?? "タスク"}>`).join('\n')
+    });
 }
 
 main();
