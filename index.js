5e1985b760be47262bff00b7d56986368a8ccbc0
tenmakamatani <tenmakamatani0@gmail.com>
2022-10-27 14:28:43 +0900

    add comment

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ const { WebClient } = require('@slack/web-api');
 
 const main = async () => {
     Dotenv.config();
+
+    // Get today limit tasks from notion
     const notion = new Client({
         auth: process.env.NOTION_AUTH_KEY,
     });
-
     const res = await notion.databases.query({ database_id: process.env.NOTION_DATABASE_ID });
     const now = new Date();
     const todayKey = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
     const todayTasks = res.results.filter((result) => result.properties['期限'].date?.start === todayKey);
 
+    // Send message to slack
     const slackClient = new WebClient(process.env.SLACK_API_TOKEN);
     await slackClient.chat.postMessage({
         channel: '#biz-all',
